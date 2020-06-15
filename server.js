297ab0b7feb6aa91a1e9f4bb1ae4e2d33219e2ac
NiceNight <974476272@qq.com>
2020-06-15 22:07:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.get('/', function (request, response) {
     })
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 80, function () {
 
     var host = server.address().address
     var port = server.address().port
 
-    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+    console.log("Server running at http://%s:%s", host, port)
 
 })
 
