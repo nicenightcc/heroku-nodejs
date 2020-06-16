ea38e9e2448fc2fef144e30aae9d7efc9a06cc62
NiceNight <974476272@qq.com>
2020-06-16 21:36:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,26 +4,45 @@ const fs = require('fs');
 const url = require('url');
 const http = require('https');
 
-const server = require('http').createServer((request, response) => {
-    console.log(request.method + ': ' + request.url);
-    if (request.method === 'GET') {
-        let query = url.parse(request.url, true).query;
+function getHtml(url) {
+    return new Promise((resolve, reject) => {
         http.get(query.url, (r) => {
             let body = '';
             r.on('data', function (data) {
                 body += data;
             });
             r.on('end', function () {
-                let matches = body.match(/img\/qr\/([0-9|a-z|A-Z]+[_|-]+[0-9|a-z|A-Z]+).png/g);
-                response.writeHead(200, {
-                    'Content-Type': 'text/plain'
-                });
-                for (var url of matches) {
-                    response.write(url);
-                }
-                response.end();
+                resolve(body);
             });
         })
+    });
+}
+
+const server = require('http').createServer((request, response) => {
+    console.log(request.method + ': ' + request.url);
+    if (request.method === 'GET') {
+        let query = url.parse(request.url, true).query;
+        if (!query.url) {
+            response.end();
+            return;
+        }
+        response.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        getHtml(query.url).then(r => {
+            let matches = r.match(/img\/qr\/([0-9|a-z|A-Z]+[_|-]+[0-9|a-z|A-Z]+).png/g);
+            let promises = [];
+            let decoded = [];
+            for (let url of matches) {
+                promises.push(getHtml('https://zxing.org/w/decode?u=' + query.url + '/' + url).then(r => {
+                    let m = r.match(/<pre>([a-z]+:\/\/[0-9|a-z|A-Z|=]+)</)[1];
+                    decoded.push(m);
+                    response.write(m);
+                }));
+
+            }
+            response.end();
+        });
     }
 });
 
